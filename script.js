41c2dad6ddb5daeae8a9deb3dd2e6c2f1bd14add
Siyabonga <siyabongakhanyile76@gmail.com>
2025-07-22 07:52:20 +0200

    added nav bar

--- script.js
+++ script.js
@@ -48,3 +48,9 @@ new Chart(usersCtx, {
     },
   },
 });
+
+const toggleBtn = document.getElementById("toggleSidebar");
+const sidebar = document.getElementById("sidebar");
+toggleBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("collapsed");
+});
